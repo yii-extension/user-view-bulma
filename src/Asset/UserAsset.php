8b2e3671ae0163be12e9f8d883acc05a40db56ef
Wilmer Arambula <terabytesoftw@gmail.com>
2021-01-03 11:12:58 -0300

    Fix fontawesome icons.

--- src/Asset/UserAsset.php
+++ src/Asset/UserAsset.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Yii\Extension\User\View\Asset;
 
+use Yii\Extension\Fontawesome\Dev\Css\NpmAllAsset;
 use Yiisoft\Assets\AssetBundle;
 use Yiisoft\Yii\Bulma\Asset\BulmaAsset;
 use Yiisoft\Yii\Bulma\Asset\BulmaHelpersAsset;
@@ -21,5 +22,6 @@ final class UserAsset extends AssetBundle
     public array $depends = [
         BulmaAsset::class,
         BulmaHelpersAsset::class,
+        NpmAllAsset::class,
     ];
 }
